ccbf5eb7ed082f4c2d6b5783d5f087252fa9a741
Mitch <wabbitsnot@gmail.com>
2013-09-25 17:54:23 -0700

    fixed bad characters in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,14 +7,14 @@ class User < ActiveRecord::Base
   has_many :posts
 
   ROLES = %w[member moderator admin]
-  def role?(base_role)
-    role.nil? ? false : ROLES.index(base_role.to_s) <= ROLES.index(role)
-  end
+  def role?(base_role)
+    role.nil? ? false : ROLES.index(base_role.to_s) <= ROLES.index(role)
+  end
 
-  private
+  private
 
-  def set_member
-    self.role = 'member'
-  end
+  def set_member
+    self.role = 'member'
+  end
 
 end
