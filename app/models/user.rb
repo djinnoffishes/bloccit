6e9f22720e01efc3bef152516f134219e12a0c85
Mitch <wabbitsnot@gmail.com>
2013-09-25 17:32:11 -0700

    fix to user models

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,16 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   has_many :posts
+
+  ROLES = %w[member moderator admin]
+  def role?(base_role)
+    role.nil? ? false : ROLES.index(base_role.to_s) <= ROLES.index(role)
+  end
+
+  private
+
+  def set_member
+    self.role = 'member'
+  end
+
 end
