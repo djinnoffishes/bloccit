a0547af20155848e2807f8a550fd6479333abbfb
Mitch <wabbitsnot@gmail.com>
2013-10-07 16:18:35 -0700

    fixed comments and added destroy actions for post topic and comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,7 +12,23 @@ class CommentsController < ApplicationController
       redirect_to topic_post_path(@comment.post.topic, @comment.post)
     else
       flash[:error] = "There was an error saving the comment. Please try again."
-      render 
+      redirect_to topic_post_path(@comment.post.topic, @comment.post)
+    end
+  end
+  
+  def destroy
+    @topic = Topic.find(params[:topic_id])
+    @post = @topic.posts.find(params[:post_id])
+
+    @comment = @post.comments.find(params[:id])
+
+    authorize! :destroy, @comment, message: "You need to own the comment to delete it."
+    if @comment.destroy
+      flash[:notice] = "Comment was removed."
+      redirect_to [@topic, @post]
+    else
+      flash[:error] = "Comment couldn't be deleted. Try again."
+      redirect_to [@topic, @post]
     end
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Bloccit::Application.routes.draw do
 
   get "comments/create"
+
   resources :topics do
     resources :posts, except: [:index] do
-      resources :comments, only: [:create]
+      resources :comments, only: [:create, :destroy]
     end
   end
+
   devise_for :users
   match 'about' => 'welcome#about', via: :get
   root to: 'welcome#index'
